9083f2be6d6074abea661c43907fc4530db7edcf
metiftikci <metiftikci@hotmail.com>
2022-06-24 12:29:02 +0300

    Split telescope settings

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,15 +1,8 @@
 require "plugins"
-
 require "lsp"
-
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
-
+require "telescope"
 require("indent_blankline").setup {
   show_current_context = true,
   show_current_context_start = true,
 }
-
 require "settings"
