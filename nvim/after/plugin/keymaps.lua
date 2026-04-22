f47d850a7f6bccb920081176eb17684baecdc273
metiftikci <metiftikci@hotmail.com>
2026-04-22 16:56:47 +0300

    Add keymap for opening files with DOS format

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -2,3 +2,4 @@ local opts = { noremap = true, silent = true }
 
 vim.api.nvim_set_keymap('n', '<leader>hh', '<cmd>noh<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>x', '<cmd>Ex<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>ed', '<cmd>e ++ff=dos<CR>', opts)
