ca8d10b5362573f70ac198fa8269b6669f174220
metiftikci <metiftikci@hotmail.com>
2026-06-17 12:37:19 +0300

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
   { src = "https://github.com/nvim-lua/plenary.nvim" },
 
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/nvim-telescope/telescope-live-grep-args.nvim" },
@@ -9,7 +9,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
 
   { src = "https://github.com/neovim/nvim-lspconfig" },
-  -- { src = "https://github.com/seblyng/roslyn.nvim" },
+  { src = "https://github.com/seblyng/roslyn.nvim" },
   { src = "https://github.com/j-hui/fidget.nvim" },
   { src = "https://github.com/hrsh7th/nvim-cmp" },
   { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
@@ -26,7 +26,7 @@ vim.pack.add({
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
 
   { src = "https://github.com/ojroques/nvim-hardline" },
-  { src = "https://github.com/RRethy/vim-illuminate" },
+  { src = "https://github.com/nvim-mini/mini.cursorword" },
   { src = "https://github.com/metiftikci/todo.vim" },
 
   { src = "https://github.com/sainnhe/sonokai" },
